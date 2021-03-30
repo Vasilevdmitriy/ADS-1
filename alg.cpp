def35dd13feeb8d193d061246bf8691b1becdf11
Vasilevdmitriy <78785147+Vasilevdmitriy@users.noreply.github.com>
2021-03-30 16:21:46 +0300

    Update alg.cpp

--- alg.cpp
+++ alg.cpp
@@ -1,1 +1,18 @@
 
+int cbinsearch(int *arr, int size, int value) {
+      int mid;
+  int l = 0;
+  int r = size - 1;
+  int count = 0;
+
+  while(l <= r){
+      mid = l + ( r - l ) / 2;
+    if (arr[mid] == value) count++;
+
+    if (arr[mid] > value) r = mid-1; 
+
+    else l = mid + 1;
+  }
+     return count;
+  return 0; // если ничего не найдено
+}
