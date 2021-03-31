8b27fe842bbd490bda637d6b977d83041ab2694c
Vasilevdmitriy <78785147+Vasilevdmitriy@users.noreply.github.com>
2021-03-31 16:31:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,42 +1,41 @@
-#include<iostream>
+// Copyright 2021 NNTU-CS
+
 int cbinsearch(int *arr, int size, int value) {
 int count = 0;
-int l = 0; 
-int r = size - 1;
+int l = 0;
+int r = size;
 int med;
 int index;
-	
-	while (l <= r)
-	{
-		med = l + (r - l) / 2;
-		if (arr[med]>value){
-			r = med-1;
-		}
-		else if (arr[med]<value){
-			l = med+1;
-		}
-		else if (arr[med]==value){
-			index = med;
-			break;
-		}
-	}
-	
-	if(index>=0){for (int i = index + 1; i < size; i++) {
-		if (arr[i] == value) {
-			count++;
-		}
-	}
-	}
-	
-	if(index<size){for (int i = index; i >= 0; i--) {
-		if (arr[i] == value) {
-			count++;
-	}
-	}
-	}
-	
-	
-	
-	if(count > 0)
-	return count;
+while (l <= r) {
+med = l + (r - l) / 2;
+if (arr[med] > value) {
+r = med - 1;
+} else if (arr[med] < value) {
+l = med + 1;
+} else if (arr[med] == value) {
+index = med;
+break;
+} else {
+return 0;
+}
+}
+if (index >= 0) {
+for (int i = index; i >= 0; i--) {
+if (arr[i] == value) {
+count++;
+}
+}
+}
+if (size > index) {
+for (int i = index + 1; i < size; i++) {
+if (arr[i] == value) {
+count++;
+}
+}
+}
+return count;
+}
+}
+if(count > 0)
+return count;
 }
